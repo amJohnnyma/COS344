cfff6b3631550dfb68cdf5b9c2e03d8e4152cb28
amJohnnyma <dewaldcoleskydc@gmail.com>
2026-02-08 10:00:41 +0200

    Initial commit

---
+++ Test/build/CMakeFiles/MyOpenGLProject.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyOpenGLProject.dir/src/glad.c.o"
+  "CMakeFiles/MyOpenGLProject.dir/src/glad.c.o.d"
+  "CMakeFiles/MyOpenGLProject.dir/src/main.cpp.o"
+  "CMakeFiles/MyOpenGLProject.dir/src/main.cpp.o.d"
+  "CMakeFiles/MyOpenGLProject.dir/src/shader.cpp.o"
+  "CMakeFiles/MyOpenGLProject.dir/src/shader.cpp.o.d"
+  "bin/MyOpenGLProject"
+  "bin/MyOpenGLProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/MyOpenGLProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
