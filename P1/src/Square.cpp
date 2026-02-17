1335e6c8f9e76be46f86c7f82baa7dbcf1093768
amJohnnyma <dewaldcoleskydc@gmail.com>
2026-02-17 08:53:59 +0200

    Skeleton cpp

--- P1/src/Square.cpp
+++ P1/src/Square.cpp
@@ -1,1 +1,37 @@
 #include "../include/Square.h"
+
+template<int n>
+Square<n>::Square(const Vector<n>& center, float height, float width) 
+{
+
+}
+template<int n>
+Square<n>::Square(const Vector<n>& tl, const Vector<n>& tr, const Vector<n>& br, const Vector<n>& bl) 
+{
+
+}
+template<int n>
+Square<n>::Square(const Square<n>&) 
+{
+
+}
+template<int n>
+Square<n>& Square<n>::operator*=(const Matrix<n,n>&) 
+{
+
+}
+template<int n>
+Square<n>* Square<n>::operator*(const Matrix<n,n>&) const 
+{
+
+}
+template<int n>
+float* Square<n>::getPoints() const 
+{
+
+}
+template<int n>
+int Square<n>::getNumPoints() const 
+{
+
+}
